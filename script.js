97370e0cdb941e26f626e1b062d82a875e0869de
Sanjero20 <gelosantos72@gmail.com>
2022-07-12 00:45:15 +0800

    Add button functionality to add a book info container

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 let myLibrary = [];
 
-function Book() {
-  // constructor
+function Book(title, author, pages, read) {
+  this.title  = title;
+  this.author = author;
+  this.pages  = pages;
+  this.read   = read;
 }
 
 function addBookToLibrary() {
   // do stuff here
 }
+
+const addBook =  document.getElementById('addBook')
+const books = document.querySelector('.books');
+
+addBook.addEventListener('click', () => {
+  const div = document.createElement('div');
+  div.classList.add('book-info');
+  books.appendChild(div)
+})
