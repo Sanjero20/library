95cf90c8115da637741903b9f7484a2edfd660f2
Sanjero20 <gelosantos72@gmail.com>
2022-07-12 22:53:42 +0800

    Add function to retrieve form inputs

--- script.js
+++ script.js
@@ -11,32 +11,35 @@ function addBookToLibrary() {
   // do stuff here
 }
 
+const $form   = document.getElementById('bookForm')
 const addBook =  document.getElementById('addBook')
-const books = document.querySelector('.books');
+const books   = document.querySelector('.books');
 
-// addBook.addEventListener('click', () => {
-//   const div = document.createElement('div');
-//   div.classList.add('book-info');
-//   books.appendChild(div)
-// })
+const modal     = document.getElementById('myModal');
+const cancelBtn = document.getElementById('cancel');
+
+const pages     = document.querySelector('.number');
+const decrement = document.querySelector('.decrement');
+const increment = document.querySelector('.increment');
+
+const submit = document.querySelector('#submit');
+
+// Prevent the site from refreshing when submit is clicked
+$form.addEventListener('submit', (e) => {
+  e.preventDefault();
+})
 
 // Modal popup
-const closeBtn = document.getElementById('cancel');
-const modal = document.getElementById('myModal');
-
 addBook.addEventListener('click', () => {
   modal.classList.add('show')
 })
 
-closeBtn.addEventListener('click', () => {
+cancelBtn.addEventListener('click', () => {
+  resetForm();
   modal.classList.remove('show')
 })
 
-// Page number 
-const pages = document.querySelector('.number');
-const decrement = document.querySelector('.decrement');
-const increment = document.querySelector('.increment');
-
+// Page number buttons
 decrement.addEventListener('click', () => {
   if (pages.value == 0) return 
   let number = Number(pages.value) - 1;
@@ -47,3 +50,34 @@ increment.addEventListener('click', () => {
   let number = Number(pages.value) + 1;
   pages.value = number;
 })
+
+// Submit button create a new book 
+submit.addEventListener('click', () => {
+  retrieveFormData();
+  resetForm();
+})
+
+
+// Functions
+function resetForm() {
+  const inputs = document.querySelectorAll('input')
+  inputs.forEach(input => {
+    input.value = "";
+    if (input.type == 'number') {
+      input.value = 0;
+    }
+  })
+}
+
+function retrieveFormData() {
+  // Returns an object consisting the datas
+  const title  = document.querySelector('[name="title"]').value;
+  const author = document.querySelector('[name="author"]').value;
+  const pages  = document.querySelector('[name="pages"]').value;
+  const status = document.querySelector('[name="read-status"]').checked;
+
+  if (title === "" || author === "" || pages === 0) return;
+
+  const bookInfo = new Book(title, author, pages, status);
+  console.log(bookInfo)
+}
