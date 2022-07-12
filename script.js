899bda7f2b8988a04cf9dd97c39641fe288b5714
Sanjero20 <gelosantos72@gmail.com>
2022-07-12 14:17:56 +0800

    Add popup modal

--- script.js
+++ script.js
@@ -14,8 +14,36 @@ function addBookToLibrary() {
 const addBook =  document.getElementById('addBook')
 const books = document.querySelector('.books');
 
+// addBook.addEventListener('click', () => {
+//   const div = document.createElement('div');
+//   div.classList.add('book-info');
+//   books.appendChild(div)
+// })
+
+// Modal popup
+const closeBtn = document.getElementById('cancel');
+const modal = document.getElementById('myModal');
+
 addBook.addEventListener('click', () => {
-  const div = document.createElement('div');
-  div.classList.add('book-info');
-  books.appendChild(div)
+  modal.classList.add('show')
+})
+
+closeBtn.addEventListener('click', () => {
+  modal.classList.remove('show')
+})
+
+// Page number 
+const pages = document.querySelector('.number');
+const decrement = document.querySelector('.decrement');
+const increment = document.querySelector('.increment');
+
+decrement.addEventListener('click', () => {
+  if (pages.value == 0) return 
+  let number = Number(pages.value) - 1;
+  pages.value = number;
+})
+
+increment.addEventListener('click', () => {
+  let number = Number(pages.value) + 1;
+  pages.value = number;
 })
