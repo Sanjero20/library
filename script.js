ca31032e29df0b36734fcdd9c76a62769c9702b1
Sanjero20 <gelosantos72@gmail.com>
2022-07-13 16:06:55 +0800

    Add a feature to add a book and update library

--- script.js
+++ script.js
@@ -1,14 +1,47 @@
+const editSVG = `<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="currentColor" d="M6 2C4.9 2 4 2.9 4 4V20C4 21.1 4.9 22 6 22H10V20.1L20 10.1V8L14 2H6M13 3.5L18.5 9H13V3.5M20.1 13C20 13 19.8 13.1 19.7 13.2L18.7 14.2L20.8 16.3L21.8 15.3C22 15.1 22 14.7 21.8 14.5L20.5 13.2C20.4 13.1 20.3 13 20.1 13M18.1 14.8L12 20.9V23H14.1L20.2 16.9L18.1 14.8Z" /></svg>`;
+const deleteSVG = `<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="currentColor" d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" /></svg>`;
+
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, status) {
   this.title  = title;
   this.author = author;
   this.pages  = pages;
-  this.read   = read;
+  this.read   = status;
+
+  this.HTMLTemplate = `
+      <h2 class="title">${this.title}</h2>
+      <hr>
+      <p class="author"> By ${this.author}</p>
+      <p class="pages">${this.pages} pages</p>
+      <div class="read">Read</div>
+
+      <section class="edit-book">
+        <div class="edit">${editSVG}</div>
+        <div class="delete">${deleteSVG}</div>
+      </section>
+    `;
+
+  this.createDiv = function() {
+    const div = document.createElement("div");
+    div.className = "book-info";
+    div.innerHTML = this.HTMLTemplate
+    return div;
+  }
 }
 
-function addBookToLibrary() {
-  // do stuff here
+function addBookToLibrary(book) {
+  myLibrary.push(book)
+}
+
+function updateLibrary() {
+  const bookContainer = document.querySelector('.books');
+  bookContainer.innerHTML = "";
+
+  myLibrary.forEach(book => {
+    const div = book.createDiv();
+    bookContainer.appendChild(div)
+  })
 }
 
 const $form   = document.getElementById('bookForm')
@@ -17,6 +50,7 @@ const books   = document.querySelector('.books');
 
 const modal     = document.getElementById('myModal');
 const cancelBtn = document.getElementById('cancel');
+const overlay   = document.querySelector('.overlay');
 
 const pages     = document.querySelector('.number');
 const decrement = document.querySelector('.decrement');
@@ -29,36 +63,36 @@ $form.addEventListener('submit', (e) => {
   e.preventDefault();
 })
 
-// Modal popup
-addBook.addEventListener('click', () => {
-  modal.classList.add('show')
-})
-
-cancelBtn.addEventListener('click', () => {
-  resetForm();
-  modal.classList.remove('show')
-})
-
-// Page number buttons
-decrement.addEventListener('click', () => {
-  if (pages.value == 0) return 
-  let number = Number(pages.value) - 1;
-  pages.value = number;
-})
-
-increment.addEventListener('click', () => {
-  let number = Number(pages.value) + 1;
-  pages.value = number;
-})
-
-// Submit button create a new book 
-submit.addEventListener('click', () => {
-  retrieveFormData();
-  resetForm();
-})
+addBook.onclick   = showModal;
+cancelBtn.onclick = closePopUp;
+decrement.onclick = decreasePageNumber;
+increment.onclick = increasePageNumber;
+submit.onclick    = submitForm;
 
 
 // Functions
+function showModal() {
+  modal.classList.add('show');
+  overlay.classList.add('show');
+}
+
+function closePopUp() {
+  resetForm();
+  modal.classList.remove('show');
+  overlay.classList.remove('show');
+}
+
+function decreasePageNumber() {
+  if (pages.value == 0) return 
+  let number = Number(pages.value) - 1;
+  pages.value = number;
+}
+
+function increasePageNumber() {
+  let number = Number(pages.value) + 1;
+  pages.value = number;
+}
+
 function resetForm() {
   const inputs = document.querySelectorAll('input')
   inputs.forEach(input => {
@@ -67,6 +101,15 @@ function resetForm() {
       input.value = 0;
     }
   })
+}
+
+function submitForm() {
+  const book = retrieveFormData();
+  if (!book) return
+  addBookToLibrary(book);
+  updateLibrary();
+  resetForm();
+  closePopUp();
 }
 
 function retrieveFormData() {
@@ -79,5 +122,5 @@ function retrieveFormData() {
   if (title === "" || author === "" || pages === 0) return;
 
   const bookInfo = new Book(title, author, pages, status);
-  console.log(bookInfo)
+  return bookInfo;
 }
