70821eef2231a4b218afab78696f63404831be50
Sanjero20 <gelosantos72@gmail.com>
2022-07-13 17:32:00 +0800

    Add feature save to local storage

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const editSVG = `<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="currentColor" d="M6 2C4.9 2 4 2.9 4 4V20C4 21.1 4.9 22 6 22H10V20.1L20 10.1V8L14 2H6M13 3.5L18.5 9H13V3.5M20.1 13C20 13 19.8 13.1 19.7 13.2L18.7 14.2L20.8 16.3L21.8 15.3C22 15.1 22 14.7 21.8 14.5L20.5 13.2C20.4 13.1 20.3 13 20.1 13M18.1 14.8L12 20.9V23H14.1L20.2 16.9L18.1 14.8Z" /></svg>`;
 const deleteSVG = `<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="currentColor" d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" /></svg>`;
 
-let myLibrary = [];
+let myLibrary = loadPreviousSession();
+updateLibrary();
 
 function Book(title, author, pages, status) {
   this.title  = title;
@@ -44,6 +45,7 @@ function updateLibrary() {
   })
 
   editBooks();
+  saveToLocal();
 }
 
 function editBooks() {
@@ -147,4 +149,27 @@ function retrieveFormData() {
 
   const bookInfo = new Book(title, author, pages, status);
   return bookInfo;
+}
+
+// Save to local storage
+function saveToLocal() {
+  localStorage.setItem("myLibrary", JSON.stringify(myLibrary))
+}
+
+function loadPreviousSession() {
+  const books = JSON.parse(localStorage.getItem("myLibrary"))
+  let library;
+
+  if (books) {
+    library = books.map((book) => JSONToArray(book))   
+  }
+  else {
+    library = []
+  }
+
+  return library
+}
+
+function JSONToArray(book) {
+  return new Book(book.title, book.author, book.pages, book.status)
 }
